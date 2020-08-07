4abe5a9230562c4253410a22d86ffbba8443a988
Tarig Bouazzati <tbouzza@gmail.com>
2020-08-07 09:28:20 +0200

    created the review form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @review = Review.new #=> an instance created for the form !
     @restaurant = Restaurant.find(params[:id])
   end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to @restaurant
+    else
+      render 'restaurants/show'
+    end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
